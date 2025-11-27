9c4d38087dd8f05717351c9535de09b53b9e7a12
breey <breey@LAPTOP-I4G050I8>
2025-11-26 20:04:17 -0600

    ejemplo uso de netbeans

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author breey
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola, este es el programa principal.");
+        System.out.println("Practica de Git en NetBeans.");
+    }
+    
+}
